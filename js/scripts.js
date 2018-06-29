132f5d55924d02361b64625f4b1a562d167816bd
Adam Kulpa <ad200@gazeta.pl>
2018-06-29 01:19:41 +0100

    Change js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 $(function(){
-	var carouselList = $('#carousel ul'); {
+	var carouselList = $('#carousel ul'); 
 		
 	setInterval(changeSlide, 3000);  
 	
@@ -8,15 +8,16 @@ $(function(){
 		
 	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);	
 	
-		$(function moveFirstSlide(){
+		function moveFirstSlide(){
 			var firstItem = carouselList.find('li:first');
 			var lastItem = carouselList.find('li:last');
 			
 			lastItem.after(firstItem)
 			carouselList.css({marginLeft:0});
-		});
-	});	
-});	
+		};	
+	};
+});
+	
 
 				
 		
